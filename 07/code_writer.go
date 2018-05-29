81d58acc5e6ae1533f8b17dded0da4da243c5e68
nirasan <niranirao@gmail.com>
2018-05-29 12:46:34 +0900

    fmt

--- 07/code_writer.go
+++ 07/code_writer.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-	"io"
 	"fmt"
+	"io"
 	"path"
 	"strings"
 )
 
 var baseSymbolMap = map[string]string{
-	"local": "LCL",
+	"local":    "LCL",
 	"argument": "ARG",
-	"this": "THIS",
-	"that": "THAT",
-	"pointer": "R3",
-	"temp": "R5",
+	"this":     "THIS",
+	"that":     "THAT",
+	"pointer":  "R3",
+	"temp":     "R5",
 }
 
 type CodeWriter struct {
-	w io.Writer
-	filename string
+	w          io.Writer
+	filename   string
 	lineNumber uint64
 }
 
@@ -34,57 +34,57 @@ func (c *CodeWriter) WriteArithmetic(command string) {
 	c.l("// " + command)
 	switch command {
 	case "add":
-		c.p("@SP") // A = 0
+		c.p("@SP")   // A = 0
 		c.p("A=M-1") // A = M[0] - 1
-		c.p("D=M") // D = M[SP-1]
+		c.p("D=M")   // D = M[SP-1]
 		c.p("A=A-1") // A = SP - 2
 		c.p("M=D+M") // M[SP-2] = M[SP-1] + M[SP-2]
 		c.p("D=A+1") // D = SP - 1
-		c.p("@SP") // A = 0
-		c.p("M=D") // M[0] = SP - 1
+		c.p("@SP")   // A = 0
+		c.p("M=D")   // M[0] = SP - 1
 	case "sub":
-		c.p("@SP") // A = 0
+		c.p("@SP")   // A = 0
 		c.p("A=M-1") // A = M[0] - 1
-		c.p("D=M") // D = M[SP-1]
+		c.p("D=M")   // D = M[SP-1]
 		c.p("A=A-1") // A = SP - 2
 		c.p("M=M-D") // M[SP-2] = M[SP-2] - M[SP-1]
 		c.p("D=A+1") // D = SP - 1
-		c.p("@SP") // A = 0
-		c.p("M=D") // M[0] = SP - 1
+		c.p("@SP")   // A = 0
+		c.p("M=D")   // M[0] = SP - 1
 	case "and":
-		c.p("@SP") // A = 0
+		c.p("@SP")   // A = 0
 		c.p("A=M-1") // A = M[0] - 1
-		c.p("D=M") // D = M[SP-1]
+		c.p("D=M")   // D = M[SP-1]
 		c.p("A=A-1") // A = SP - 2
 		c.p("M=D&M") // M[SP-2] = M[SP-2] | M[SP-1]
 		c.p("D=A+1") // D = SP - 1
-		c.p("@SP") // A = 0
-		c.p("M=D") // M[0] = SP - 1
+		c.p("@SP")   // A = 0
+		c.p("M=D")   // M[0] = SP - 1
 	case "or":
-		c.p("@SP") // A = 0
+		c.p("@SP")   // A = 0
 		c.p("A=M-1") // A = M[0] - 1
-		c.p("D=M") // D = M[SP-1]
+		c.p("D=M")   // D = M[SP-1]
 		c.p("A=A-1") // A = SP - 2
 		c.p("M=D|M") // M[SP-2] = M[SP-2] & M[SP-1]
 		c.p("D=A+1") // D = SP - 1
-		c.p("@SP") // A = 0
-		c.p("M=D") // M[0] = SP - 1
+		c.p("@SP")   // A = 0
+		c.p("M=D")   // M[0] = SP - 1
 	case "neg":
-		c.p("@SP") // A = 0
+		c.p("@SP")   // A = 0
 		c.p("A=M-1") // A = M[0] - 1
-		c.p("M=-M") // M[SP-1] = -M[SP-1]
+		c.p("M=-M")  // M[SP-1] = -M[SP-1]
 	case "not":
-		c.p("@SP") // A = 0
+		c.p("@SP")   // A = 0
 		c.p("A=M-1") // A = M[0] - 1
-		c.p("M=!M") // M[SP-1] = !M[SP-1]
+		c.p("M=!M")  // M[SP-1] = !M[SP-1]
 	case "eq", "gt", "lt":
-		c.p("@SP") // A = 0
+		c.p("@SP")   // A = 0
 		c.p("A=M-1") // A = M[0] - 1
-		c.p("D=M") // D = M[SP-1]
+		c.p("D=M")   // D = M[SP-1]
 		c.p("A=A-1") // A = SP - 2
 		c.p("D=M-D") // D = M[SP-2] - M[SP-1]
 		// compare
-		c.p("@%d", c.lineNumber + 8) // A = true statement
+		c.p("@%d", c.lineNumber+8) // A = true statement
 		switch command {
 		case "eq":
 			c.p("D;JEQ") // if (D = 0) goto A
@@ -95,16 +95,16 @@ func (c *CodeWriter) WriteArithmetic(command string) {
 		}
 		// set false
 		c.p("@SP")
-		c.p("A=M-1") // A = M[0] - 1 = SP - 1
-		c.p("A=A-1") // A = SP - 2
-		c.p("M=0") // M[SP-2] = false
-		c.p("@%d", c.lineNumber + 6) // A = out of if statement
-		c.p("0;JMP") // goto A
+		c.p("A=M-1")               // A = M[0] - 1 = SP - 1
+		c.p("A=A-1")               // A = SP - 2
+		c.p("M=0")                 // M[SP-2] = false
+		c.p("@%d", c.lineNumber+6) // A = out of if statement
+		c.p("0;JMP")               // goto A
 		// set true
 		c.p("@SP")
 		c.p("A=M-1") // A = M[0] - 1 = SP - 1
 		c.p("A=A-1") // A = SP - 2
-		c.p("M=-1") // M[SP-2] = true
+		c.p("M=-1")  // M[SP-2] = true
 		// set new SP
 		c.p("@SP")
 		c.p("M=M-1") // M[0] = M[0] - 1
@@ -126,16 +126,16 @@ func (c *CodeWriter) WritePushPop(command string, segment string, index string) 
 		case "local", "argument", "this", "that", "pointer", "temp":
 			// get value
 			c.p("@%s", index) // A = n
-			c.p("D=A") // D = n
+			c.p("D=A")        // D = n
 			switch segment {
 			case "local", "argument", "this", "that":
 				c.p("@%s", baseSymbolMap[segment]) // A = BASE address pointer
-				c.p("A=M") // A = M[BASE address pointer] = BASE
+				c.p("A=M")                         // A = M[BASE address pointer] = BASE
 			case "pointer", "temp":
 				c.p("@%s", baseSymbolMap[segment]) // A = BASE
 			}
 			c.p("A=D+A") // A = BASE + n
-			c.p("D=M") // D = M[BASE + n]
+			c.p("D=M")   // D = M[BASE + n]
 			// push
 			c.p("@SP")
 			c.p("A=M")
@@ -146,7 +146,7 @@ func (c *CodeWriter) WritePushPop(command string, segment string, index string) 
 		case "static":
 			// get value
 			c.p("@%s.%s", c.filename, index) // A = FILENAME.INDEX
-			c.p("D=M") // D = M[FILENAME.INDEX]
+			c.p("D=M")                       // D = M[FILENAME.INDEX]
 			// push
 			c.p("@SP")
 			c.p("A=M")
@@ -160,36 +160,36 @@ func (c *CodeWriter) WritePushPop(command string, segment string, index string) 
 		case "local", "argument", "this", "that", "pointer", "temp":
 			// calc local address
 			c.p("@%s", index) // A = n
-			c.p("D=A") // D = n
+			c.p("D=A")        // D = n
 			switch segment {
 			case "local", "argument", "this", "that":
 				c.p("@%s", baseSymbolMap[segment]) // A = BASE address pointer
-				c.p("A=M") // A = M[BASE address pointer] = BASE
+				c.p("A=M")                         // A = M[BASE address pointer] = BASE
 			case "pointer", "temp":
 				c.p("@%s", baseSymbolMap[segment]) // A = BASE
 			}
 			c.p("D=D+A") // D = BASE + n
-			c.p("@R13") // A = 13 (common reg)
-			c.p("M=D") // M[13] = BASE + n
+			c.p("@R13")  // A = 13 (common reg)
+			c.p("M=D")   // M[13] = BASE + n
 			// get head of stack
-			c.p("@SP") // A = 0
+			c.p("@SP")   // A = 0
 			c.p("A=M-1") // A = M[0] - 1
-			c.p("D=M") // D = M[SP-1]
+			c.p("D=M")   // D = M[SP-1]
 			// write
 			c.p("@R13") // A = 13
-			c.p("A=M") // A = BASE + n
-			c.p("M=D") // M[BASE+n] = M[SP]
+			c.p("A=M")  // A = BASE + n
+			c.p("M=D")  // M[BASE+n] = M[SP]
 			// SP--
 			c.p("@SP")
 			c.p("M=M-1")
 		case "static":
 			// get head of stack
-			c.p("@SP") // A = 0
+			c.p("@SP")   // A = 0
 			c.p("A=M-1") // A = M[0] - 1
-			c.p("D=M") // D = M[SP-1]
+			c.p("D=M")   // D = M[SP-1]
 			// write
 			c.p("@%s.%s", c.filename, index) // A = FILENAME.INDEX
-			c.p("M=D") // M[FILENAME.INDEX] = M[SP]
+			c.p("M=D")                       // M[FILENAME.INDEX] = M[SP]
 			// SP--
 			c.p("@SP")
 			c.p("M=M-1")
@@ -198,10 +198,10 @@ func (c *CodeWriter) WritePushPop(command string, segment string, index string) 
 }
 
 func (c *CodeWriter) p(format string, a ...interface{}) {
-	fmt.Fprintf(c.w, format + "\n", a...)
+	fmt.Fprintf(c.w, format+"\n", a...)
 	c.lineNumber += 1
 }
 
 func (c *CodeWriter) l(format string, a ...interface{}) {
-	fmt.Fprintf(c.w, format + "\n", a...)
+	fmt.Fprintf(c.w, format+"\n", a...)
 }
--- 07/parser.go
+++ 07/parser.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"io"
 	"bufio"
+	"io"
 	"strings"
 )
 
@@ -12,16 +12,16 @@ var (
 	}
 	commandTypeMap = map[string]CommandType{
 		`push`: C_PUSH,
-		`pop`: C_POP,
-		`add`: C_ARITHMETIC,
-		`sub`: C_ARITHMETIC,
-		`neg`: C_ARITHMETIC,
-		`eq`: C_ARITHMETIC,
-		`gt`: C_ARITHMETIC,
-		`lt`: C_ARITHMETIC,
-		`and`: C_ARITHMETIC,
-		`or`: C_ARITHMETIC,
-		`not`: C_ARITHMETIC,
+		`pop`:  C_POP,
+		`add`:  C_ARITHMETIC,
+		`sub`:  C_ARITHMETIC,
+		`neg`:  C_ARITHMETIC,
+		`eq`:   C_ARITHMETIC,
+		`gt`:   C_ARITHMETIC,
+		`lt`:   C_ARITHMETIC,
+		`and`:  C_ARITHMETIC,
+		`or`:   C_ARITHMETIC,
+		`not`:  C_ARITHMETIC,
 	}
 )
 
